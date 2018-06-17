19f767e1e15904e6e7f49991aa792dcf92c9ae22
Herb Peyerl <hpeyerl+gitlab@beer.org>
2018-06-17 11:39:52 +0000

    return some readings

--- src/airthingswave-mqtt/__main__.py
+++ src/airthingswave-mqtt/__main__.py
@@ -15,8 +15,15 @@ def test():
     print ("Config file: {0}".format(c))
 
     atw=airthingswave.AirthingsWave_mqtt(c)
-    print(atw.config)
 
+    count=len(atw.waves)
+    if count > 0:
+        iter=0
+	while iter<count:
+            print(atw.waves[iter]["name"], atw.waves[iter]["addr"])
+            bqm3, temp, humidity, date = atw.get_reading(atw.waves[iter]["addr"])
+            print("{0} says {1}, {2}, {3}, {4}".format(atw.waves[iter]["name"],date, temp, humidity, bqm3))
+            iter = iter+1
     return True
 
 test()
--- src/airthingswave-mqtt/airthingswave.py
+++ src/airthingswave-mqtt/airthingswave.py
@@ -1,7 +1,71 @@
 import yaml
 import logging
+import paho.mqtt.client as mqtt
+from bluepy.btle import UUID, Peripheral
+from datetime import datetime
+import sys
+import time
+import struct
+import re
+
+class Sensor:
+    def __init__(self, name, uuid, format_type, unit, scale):
+        self.name = name
+        self.uuid = uuid
+        self.format_type = format_type
+        self.unit = unit
+        self.scale = scale
 
 class AirthingsWave_mqtt:
     def __init__(self, config_file):
-	with open(config_file, 'r') as f:
-		self.config=yaml.load(f)
+        with open(config_file, 'r') as f:
+            self.config=yaml.load(f)
+        self.waves=list()
+        if self.check_config(self.config):
+            self.mqtt_client = mqtt.Client()
+        self.sensors = []
+        self.sensors.append(Sensor("DateTime", UUID(0x2A08), 'HBBBBB', "\t", 0))
+        self.sensors.append(Sensor("Temperature", UUID(0x2A6E), 'h', "deg C\t", 1.0/100.0))
+        self.sensors.append(Sensor("Humidity", UUID(0x2A6F), 'H', "%\t\t", 1.0/100.0))
+        self.sensors.append(Sensor("Radon 24h avg", "b42e01aa-ade7-11e4-89d3-123b93f75cba", 'H', "Bq/m3\t", 1.0))
+        self.sensors.append(Sensor("Radon long term", "b42e0a4c-ade7-11e4-89d3-123b93f75cba", 'H', "Bq/m3\t", 1.0))
+
+    def check_config(self, conf):
+        if "mqtt" not in conf:
+            return false
+        if "broker" not in conf["mqtt"]:
+            return false
+        if "port" not in conf["mqtt"]:
+            return false
+        if "waves" in conf:
+            for wave in conf["waves"]:
+                if "addr" in wave and "name" in wave:
+                    self.waves.append(wave)
+                else:
+                    print("Malformed config item: {0}".format(wave))
+
+    def mqtt_connect(self, conf):
+	self.mqtt_conf=self.config["mqtt"]
+	self.mqtt_client.connect(conf["broker"], conf["port"])
+
+    def ble_connect(self, addr):
+	p = Peripheral(addr)
+	return p
+
+    def ble_disconnect(self, p)
+       p.disconnect()
+
+    def get_readings(self, p, addr):
+        str_out = ""
+        for s in self.sensors:
+            ch  = p.getCharacteristics(uuid=s.uuid)[0]
+            if (ch.supportsRead()):
+                val = ch.read()
+                val = struct.unpack(s.format_type, val)
+                if s.name == "DateTime":
+                    str_out += str(datetime(val[0], val[1], val[2], val[3], val[4], val[5])) + s.unit
+                else:
+                    str_out += str(val[0] * s.scale) + " " + s.unit
+		readings[s.name]=str_out
+	
+        return readings
